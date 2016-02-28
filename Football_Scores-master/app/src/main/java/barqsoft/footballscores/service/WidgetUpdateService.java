4278982f6d4680aeda68a307d2c906cc1c73e667
Lokesh Tiwari <tiwari9773@gmail.com>
2016-02-28 18:57:19 +0530

    style: widget can be resized and adjusted by user . User can see widget in small screen also . Add Content description for widget views

--- Football_Scores-master/app/src/main/java/barqsoft/footballscores/service/WidgetUpdateService.java
+++ Football_Scores-master/app/src/main/java/barqsoft/footballscores/service/WidgetUpdateService.java
@@ -1,5 +1,6 @@
 package barqsoft.footballscores.service;
 
+import android.annotation.TargetApi;
 import android.app.IntentService;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
@@ -7,6 +8,11 @@ import android.content.ComponentName;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.util.TypedValue;
 import android.widget.RemoteViews;
 
 import java.text.SimpleDateFormat;
@@ -15,7 +21,6 @@ import java.util.Date;
 
 import barqsoft.footballscores.DatabaseContract;
 import barqsoft.footballscores.MainActivity;
-import barqsoft.footballscores.MainScreenFragment;
 import barqsoft.footballscores.R;
 import barqsoft.footballscores.Utilies;
 import barqsoft.footballscores.widget.TodaysMatchProvider;
@@ -24,6 +29,8 @@ import barqsoft.footballscores.widget.TodaysMatchProvider;
  * Created by Lokesh on 25/02/2016.
  */
 public class WidgetUpdateService extends IntentService {
+
+    private static final String TAG = WidgetUpdateService.class.getName();
 
     private static final String[] SCORE_COLUMNS = {
             DatabaseContract.scores_table.MATCH_ID,
@@ -34,7 +41,6 @@ public class WidgetUpdateService extends IntentService {
             DatabaseContract.scores_table.LEAGUE_COL,
             DatabaseContract.scores_table.TIME_COL
     };
-
     // these indices must match the projection
     private static final int INDEX_MATCH_ID = 0;
     private static final int INDEX_HOME_COL = 1;
@@ -96,12 +102,50 @@ public class WidgetUpdateService extends IntentService {
         data.moveToPosition(mark);
 
         for (int appWidgetId : appWidgetIds) {
-            int layoutId = R.layout.widget_today;
 
+            String time = data.getString(INDEX_TIME_COL);
+
+            String strHomeTeam = data.getString(INDEX_HOME_COL);
+            String strAwayTeam = data.getString(INDEX_AWAY_COL);
+            // Find the correct layout based on the widget's width
+            int widgetWidth = getWidgetWidth(appWidgetManager, appWidgetId);
+            int defaultWidth = getResources().getDimensionPixelSize(R.dimen.widget_today_default_width);
+            int largeWidth = getResources().getDimensionPixelSize(R.dimen.widget_today_large_width);
+            int layoutId;
+            if (widgetWidth >= largeWidth) {
+                layoutId = R.layout.widget_today_large;
+
+                time = getString(R.string.msg_match_start_time, time);
+            } else {
+                layoutId = R.layout.widget_today;
+
+                /*Define short string for small widget*/
+                time = getString(R.string.msg_match_start_time_short, time);
+
+                /*Lets make name abbreviated due to space constraints*/
+                String[] arrHomeTeam = strHomeTeam.split(" ");
+                String temp = "";
+                for (String s : arrHomeTeam) {
+                    temp = temp + s.substring(0, 1);
+                }
+                Log.i(TAG, "onHandleIntent: HOME-TEAM " + strHomeTeam + "(" + temp + ")");
+                strHomeTeam = temp;
+
+
+                /*Clean the temp variable*/
+                temp = "";
+                String[] arrAwayTeam = strAwayTeam.split(" ");
+                for (String s : arrAwayTeam) {
+                    temp = temp + s.substring(0, 1);
+                }
+                Log.i(TAG, "onHandleIntent: HOME-TEAM " + strAwayTeam + "(" + temp + ")");
+                strAwayTeam = temp;
+            }
 
             RemoteViews remoteViews = new RemoteViews(getPackageName(), layoutId);
-            remoteViews.setTextViewText(R.id.widget_home_team, data.getString(INDEX_HOME_COL));
-            remoteViews.setTextViewText(R.id.widget_away_team, data.getString(INDEX_AWAY_COL));
+            remoteViews.setTextViewText(R.id.widget_home_team, strHomeTeam);
+
+            remoteViews.setTextViewText(R.id.widget_away_team, strAwayTeam);
 
             String home_goal = data.getString(INDEX_HOME_GOAL_COL);
             String away_goal = data.getString(INDEX_AWAY_GOAL_COL);
@@ -113,13 +157,21 @@ public class WidgetUpdateService extends IntentService {
                 away_goal = "-";
             }
 
+            remoteViews.setTextViewText(R.id.tv_widget_match_start_time, time);
             remoteViews.setTextViewText(R.id.widget_home_team_score, home_goal);
             remoteViews.setTextViewText(R.id.widget_away_team_score, away_goal);
 
-            String time = data.getString(INDEX_TIME_COL);
+            // Content Descriptions for RemoteViews were only added in ICS MR1
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1) {
+                setRemoteContentDescription(remoteViews, R.id.tv_widget_match_start_time, time);
+                setRemoteContentDescription(remoteViews, R.id.widget_home_team, data.getString(INDEX_HOME_COL));
+                setRemoteContentDescription(remoteViews, R.id.widget_away_team, data.getString(INDEX_AWAY_COL));
 
-            time = getString(R.string.msg_match_start_time, time);
-            remoteViews.setTextViewText(R.id.tv_widget_match_start_time, time);
+                String msgHomeGoal = getString(R.string.msg_goal_stat, data.getString(INDEX_HOME_COL), data.getString(INDEX_HOME_GOAL_COL));
+                String msgAwayGoal = getString(R.string.msg_goal_stat, data.getString(INDEX_AWAY_COL), data.getString(INDEX_AWAY_GOAL_COL));
+                setRemoteContentDescription(remoteViews, R.id.widget_home_team_score, msgHomeGoal);
+                setRemoteContentDescription(remoteViews, R.id.widget_away_team_score, msgAwayGoal);
+            }
 
             // Create an Intent to launch MainActivity
             Intent launchIntent = new Intent(this, MainActivity.class);
@@ -130,5 +182,33 @@ public class WidgetUpdateService extends IntentService {
             appWidgetManager.updateAppWidget(appWidgetId, remoteViews);
         }
 
+    }
+
+    private int getWidgetWidth(AppWidgetManager appWidgetManager, int appWidgetId) {
+        // Prior to Jelly Bean, widgets were always their default size
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
+            return getResources().getDimensionPixelSize(R.dimen.widget_today_default_width);
+        }
+        // For Jelly Bean and higher devices, widgets can be resized - the current size can be
+        // retrieved from the newly added App Widget Options
+        return getWidgetWidthFromOptions(appWidgetManager, appWidgetId);
+    }
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    private int getWidgetWidthFromOptions(AppWidgetManager appWidgetManager, int appWidgetId) {
+        Bundle options = appWidgetManager.getAppWidgetOptions(appWidgetId);
+        if (options.containsKey(AppWidgetManager.OPTION_APPWIDGET_MIN_WIDTH)) {
+            int minWidthDp = options.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_WIDTH);
+            // The width returned is in dp, but we'll convert it to pixels to match the other widths
+            DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+            return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, minWidthDp,
+                    displayMetrics);
+        }
+        return getResources().getDimensionPixelSize(R.dimen.widget_today_default_width);
+    }
+
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1)
+    private void setRemoteContentDescription(RemoteViews views, int id, String description) {
+        views.setContentDescription(id, description);
     }
 }
